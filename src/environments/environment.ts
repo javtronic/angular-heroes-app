9835725f96564725b39e121bc00a492e354ff9af
javtronic <juniorjavi7@gmail.com>
2023-08-24 18:13:59 -0500

    Fin clase 14 - firt commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+// ya no viene en versiones de angular +
 
 export const environment = {
-  production: false
+  production: false,
+  baseURL: 'http://localhost:3000'
 };
 
 /*
